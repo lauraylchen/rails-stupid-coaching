9b66de230503b3e8eb248c664ee8ca013d2422b2
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-14 13:45:31 -0500

    Completed Stupid Coaching Challenge without Optional

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+# Method Question Controller
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @answer = ['Great !', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+    @question = params[:question]
+
+    @answer = if @question == 'I am going to work'
+                @answer.first
+              elsif @question.include?('?')
+                @answer[1]
+              else
+                @answer.last
+              end
+  end
+end
